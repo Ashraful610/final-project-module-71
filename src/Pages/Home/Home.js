49221c5b992a556681d6179e95635415a942acd3
Ashraful610 <ashalam610@gmail.com>
2022-05-24 13:34:48 +0600

    card-done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Home.css'
+import Banner from './Banner';
+import Info from './Info';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Welcome to home page</h2>
+        <div className="home">
+            <Banner></Banner>
+            <Info></Info>
         </div>
     );
 };
---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
+
+const Info = () => {
+    return (
+        <div className="grid grid-cols-1  lg:grid-cols-3 gap-5 mt-10 mb-10">
+            <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' text='We provide service this time'  cardTitle='Opening Hour' img={clock}></InfoCard>
+
+            <InfoCard bgClass='bg-accent' text='Dhaka , Bangladesh' cardTitle='Visited Our Location' img={marker}></InfoCard>
+
+            <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' text='+0024840384' cardTitle='Contact us Now'  img={phone}></InfoCard>
+        </div>
+    );
+};
+
+export default Info;
