b355d6dd6af72654502cb0382873398ff3f78cc0
Ashraful610 <ashalam610@gmail.com>
2022-05-25 20:57:46 +0600

    done

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Service = ({service , setTreatment}) => {
     const {name , slots} = service
     return (
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-            <h2 className="text-2xl text-center">{name}</h2>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body my-5">
+            <h2 className="text-2xl text-center text-secondary">{name}</h2>
             <p className='text-center'>
                 {
                     slots.length ? <span>{slots[0]}</span> : <span className='text-red-500'>Try another day</span>
@@ -14,10 +14,10 @@ const Service = ({service , setTreatment}) => {
             <p className='text-center'>
                {slots.length} {slots.length > 1 ? 'spaces' :'space'} available
             </p>
-            <div class="card-actions justify-center">
+            <div className="card-actions justify-center">
                 <label 
                     for="booking-modal"
-                    className="btn btn-secondary text-white uppercase"
+                    className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
                     disabled={slots.length === 0}
                     onClick={()=> setTreatment(service)}>
                      Book Appointment
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if(user){
+        console.log(user)
+    }
     return (
-        <div>
-            <h3>Please log in</h3>
+         <div className='flex justify-center items-center h-screen'>
+             <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-2xl font-bold text-primary text-center">Log In</h2>
+                   
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline btn-secondary">Continue with Google</button>
+                </div>
+            </div>
         </div>
     );
 };
